fdc6018fb1214d321d9273e7da7b27b538eac682
Mikhail Muzhev <mikhail.muzhev@ltst.ru>
2020-07-16 09:51:31 +0600

    Initial commit

---
+++ PetProject/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  PetProject
+//
+//  Created by Mikhail Muzhev on 16.07.2020.
+//  Copyright © 2020 Mikhail Muzhev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
